fe0b307c75ef634e50e247d507eaf4ec54ac26c7
khayaklap <156482279+khayaklap@users.noreply.github.com>
2025-12-01 14:40:37 -0500

    Add files via upload

--- external_agents/__init__.py
+++ external_agents/__init__.py
@@ -1,1 +1,12 @@
+"""
+External Agents
 
+Data collection agents for the Fed Policy Intelligence Platform.
+
+Available agents:
+- fred: Federal Reserve Economic Data
+- bls: Bureau of Labor Statistics
+- treasury: US Treasury Department
+"""
+
+__version__ = "1.0.0"
